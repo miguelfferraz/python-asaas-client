bf447fb77dc6815104f4979d09b7aadab92dbf65
Miguel Figueira Ferraz <miguelfigueiraferraz@gmail.com>
2023-11-07 12:26:46 -0300

    feat: Adds get and post methods to client

--- asaas_client/client.py
+++ asaas_client/client.py
@@ -1,5 +1,7 @@
 from typing import Dict
 
+import httpx
+
 
 class AsaasClient:
     def __init__(self, api_key: str, domain: str = "https://api.asaas.com/v3"):
@@ -12,3 +14,16 @@ class AsaasClient:
             "Accept": "application/json",
             "Access-Token": self.api_key,
         }
+
+    def _build_url(self, endpoint: str) -> str:
+        return f"{self.domain}/{endpoint}"
+
+    def get(self, endpoint: str) -> httpx.Response:
+        return httpx.get(url=self._build_url(endpoint), headers=self._default_headers)
+
+    def post(self, endpoint: str, data: Dict) -> httpx.Response:
+        return httpx.post(
+            url=self._build_url(endpoint),
+            headers=self._default_headers,
+            json=data,
+        )
