756df817f64a3b753b0cb777c6513e820751bad7
Miguel Figueira Ferraz <miguelfigueiraferraz@gmail.com>
2023-12-06 12:56:09 -0300

    test: Adds unit tests for `AsaasClient`

--- tests/test_client.py
+++ tests/test_client.py
@@ -12,36 +12,10 @@ def test_default_headers(asaas_client):
     headers = asaas_client._default_headers
     assert "Accept" in headers
     assert "Access-Token" in headers
-    assert headers["Access-Token"] == "api_key"
 
 
-def test_build_url(asaas_client):
-    builded_url = asaas_client._build_url("customers")
-
-    assert builded_url == "example.com/customers"
-
-
-def test_get_data(mocker, asaas_client):
-    mock_response = mocker.Mock()
-    mock_response.status_code = 200
-    mock_response.text = "Mocked GET request"
-
-    mocker.patch("httpx.get", return_value=mock_response)
-
-    response = asaas_client.get("orders")
-
-    assert response.status_code == 200
-    assert response.text == "Mocked GET request"
-
-
-def test_post_data(mocker, asaas_client):
-    mock_response = mocker.Mock()
-    mock_response.status_code = 201
-    mock_response.text = "Mocked POST request"
-
-    mocker.patch("httpx.post", return_value=mock_response)
-
-    response = asaas_client.post("orders", {"data": "data"})
-
-    assert response.status_code == 201
-    assert response.text == "Mocked POST request"
+def test_getattr(asaas_client):
+    name = "products"
+    asaas_request = asaas_client.__getattr__(name)
+    assert asaas_request.base_url == "example.com"
+    assert asaas_request.headers == asaas_client._default_headers
